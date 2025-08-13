930cff8ff5332e8649025a5e3b15304a66cfd298
Patrick-M-C <84278175+Patrick-M-C@users.noreply.github.com>
2025-08-13 12:41:26 +0200

    Fikset noget frontend module pathing

--- FerieRegistrering/src/app/app.module.ts
+++ FerieRegistrering/src/app/app.module.ts
@@ -1,9 +1,32 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login/login.component';
+import { AuthGuard } from './auth/auth-guard';
+import { AdminGuard } from './auth/admin-guard';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
   imports: [
-    FormsModule
-  ]
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      // her kan du tilføje flere ruter senere
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: '**', redirectTo: '/login' }
+    ])
+  ],
+  providers: [
+    AuthGuard,
+    AdminGuard
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
