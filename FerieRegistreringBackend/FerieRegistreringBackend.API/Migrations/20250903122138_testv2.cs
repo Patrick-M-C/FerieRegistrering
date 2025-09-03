504dbaf10104ac0f089e709ebf36bc05a500c148
valbon osmani <valb0043@hotmail.com>
2025-09-03 14:28:38 +0200

    ændret jwt key, så det indeholder minimum antal tegn krav

---
+++ FerieRegistreringBackend/FerieRegistreringBackend.API/Migrations/20250903122138_testv2.cs
@@ -0,0 +1,438 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FerieRegistreringBackend.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class testv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAELDiYvVgN0PP06kliDKcA3vsrCY4q1JAcrFTDYukE1e71KhkMmVaejLpZ+o5EGYRKQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEO6vX7Gr1HaWPhLk06BE/bzdBp4VvFOXdoMK3NqJU5vP1b2oxi+mqMhtbJwVe/frSQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEHjunXZImTP+/ggjNkUhQd1sl+j9X6uZE0umoQBhDwL1EdlHYapidAGMOq3HuCHTlQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEG2W6aMS7DYYSA+xBzz6VTDYYJSV0hOvsCD9cOQf97zVyhLMUyG6wFvZER0QG4QUbQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEPM+P1cXaC9gi/uqkef3F2wQq3q4+QEnIKgKnR2y14PW9Lz/BGsQs+/PGphOiPUSpw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEJQlGs0y/Hs7qqjjnepHfxZ45+aRt7cdWV6hWP6UlwQKnf8P+di0vuTAhBD0+l0JEg==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEL85YKt74o6y16O8ik+vSnj9bXyiuAwvFqIs/M+yeQukOWHITELiWmd2EPlZt+dt0g==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEIxVLfhhH1TsM+ndsJ7q13tZXmMCz356xkm2gYlTuY11WdHERUmmofD89NBYlyTeJQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEIs1gZ+ziEKXLlaWrW1qjeBChOrsErftGhx7Mp016RXRTeXQlWeRalDT25BdTjIr0A==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGIPZQs69ta8MJ4Vr1v4KEzpYlIXMoGy7xiDnXNZpc/ZdwHro5ESJWhzg3OryqnYIA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEHZPeyUHZmM32eWyFqhb9CN9RxNbJwmWd4BT3c4o0Ip2g9taVptTNfwcAcv0AuaUFA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEJ7CbOCliOJTncY/5EabvWn0gdQT4FU/dXcenE60X8ixJw/nYeNN+T/+vEHZyBEzMA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGlHHJVi8fHkNsbvtZ7usFWgy0kU5rVPNTlbUZ2PHxp+eASXZDg28vkVtcFrSKa97w==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEANGPeg35L69YK3GwRaCYbiKAP421OpwlPlMw6FUhZf5EmdTvxCOfy7DNDzJiqWBoQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEKZ9fr60gALwcO9jtPqAetJ7aky3jrKMQs85hihyUWampzvW/oW6pwA9LkdiVmPxkw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEI5CQL4R002T/MljHizvF+zMsH0euig+3gnrEOs2Ax3biP5YMChafslEYUWZbqzbyA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAELZKBZYGOnEPmnGxVlMH51fWF5jmjGgkLmegvMoTPwixZMx/e/xOWf0i4jMA7Ai18w==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEAwcg+xSF0/fVG8wWY5H01NpTL+qJi/NGzFmnV7rlnXjwVoK8AqCRkGvgaFAb97ZLA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEBxLQ5RnsEG+2XqHm/nqpwaersb0bv9aLyG28kPLKcbJYEoaeD0D6yk/rIjTpCcccQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEA2Ryq3uv4iK4bLrkZ/KuoRwInp694CGNMKDeF/aYqhbyiQ36J2nUuhi+tYRd/93LQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGExSNB6g24w03M4KQuwNR9ggX+zeG+707Zc6RZk4qQx0zMgkr3moCu5F4pBBEFVuQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEF84m8qteXWU4WjXONYSEE3daZBxawhy2AQjYLV4og+guREUzetgpuekZtCe0YiuVg==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEFaK9iqb5voBh1cx1b7yS7ovYu1ARY0DwmTh8QgstkNkTwhLi7Lr4dt36RY3cl4ckQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEPeGlF8h2spjc9Ku9sgda4dh33x0QmbyNdtoDxL5cYGpih3qR1LrVT96Mm+R4RzByw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEHsMJen9L2Jstpo1ZgIEWC2vCxcRr33FMtSK4mrquKlAN3LSc2ET9jgm0145d5d/yw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGXK3tu8IMPKwcTzOMiMdeEgLW+gMc9QN0AsjWMi5TRhEgP6T9z+22nVIolQHk5Zfw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAENP/1kshLTlgOBTEDmLBlhNbE+mddRoHUjay+FgJEJiic4PePQXrAqOfAFCzvBdmWw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGJniq+dn+BQ886BjbL5dV757VziCcX9qPAPQSEp2Ds6CBqeDLkxJTdpOzJdlOZoVQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEPbrU159VNsb27F9UL4BM28F6ke+nRbJv3SbF5gwQIJjpmqqGmXmSrrJ/0JCkJ1LzA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAECjwxmPbNVr5JwKnmDyPiQt9jKbBVJp+XEdMVyAbFQP43p109oSzjE5Q29+r05ukVQ==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGU2X5virsaYduePQ9+lqbYZy3hDiFecSruFm9GBw8fwuR8uHT6FnVkd64QxWm5Oag==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEONVbZUhQc+iw5Vv9mlJROEzGrpRTVpG94PD4H80KPcfKaTzbdEGPAeIYjiMZm4Kew==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEB3b33zhR1mGF024RvqjCzA6m3mZGMvr2fQ6KIM1S8z45r4pSe+2VzaP1MWBYrYRoQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEEYeI3t3mJ2wQlRJbXs0PeldLgFvaVny4PatWKgQoEpP8jl9EEfzBZ/lFjhAgwlY4A==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEMfA2QyJTuJy1L+AZWY3WHvgxEEExNFj7CFRbPZTbnOX97ZQ7RbKrSI1W/140sMp1A==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEBq4cX1NDG4uyEs3v0zLQz3faR7805YcbnqYZ8yi5nRWNeabCGQ2+9XTeSrKu4y3Sw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEKw64HCi2M+fqGFzP+VfLWreW6g3OPuVr8hssx/Gy/fe7RIhQNy5wu+ayPkr662sYw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAECapunPNxGxZLHEd2hsUWJBoy6ozXX5zQLHWurboNEo8s8zXxU6aPS+em7svVy28bQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEIgXSlUCjkMotRgbuZvvwW3QXh08hMYkQ1J8oQBPFjxCBf9mMWdaOewk9h5zwsZARw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAENiA3s63t6slaJTdkdoUd8oSNDg1tK6R7PFmwGAMRUiqtDRmFFZFpjvNaCvGeZftqQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEESegEzBC59RGKcd03Qk/RMU6hK4ZPwiyi/rXvwACm4m/0dg7+8rZ5ELn/TSL34OlQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEFZa4/5WUFT/PG1aeY5Q1uAHLLTo6GU8hWmgQJFp2gx2vgxcUGhF2YGxB2XcxIo/uQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAECVFibw/44MOsGebbFCuctc2n+fCP9FETQ4avq5jMlTGSLpV711N7RNPNthMJ9ybuQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAELD59XNBBA6Z+51strwBk9Rzavgz7w1s4v63w2WsyKkNEoGOBPK3v893kAC26lX6+w==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEEIE2aXp6XsydbzdaAGNFPi+Hfs8qpTu/EQFnItKjxkFI6PVryD8Zrpnb2eviuEmLQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEFWlXzM06yQuR/zJsWsFe9t5AMVRRiBTd1G9MLA/ttJM6gfXGsCsZkpDsnEM0s+Eaw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAECkPn8ehCVRpkrQKmoRqBoEfXnPIrH+rbN6tCFn9fiah5oI3zkTDUMWzP3K3yY+OLQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEIHjxhC6g/lrAbkjXRgVG9v0PzC+WEWpOAyjNmDe2TDq2pip+uzychOaWY2zkKtSqQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEA67T2GRrhfhWyfDSjIl5pxEQ5vaUwbIs9ZYcu9EaVvtJmZ+7rZL7+B5l+/relxgmQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEELMI+lVrTlrEAIx09FdBUsj4YBUXnziw8/FxsJ5xWVJeqfkcpn0LxTnDoyLrMlAMg==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAECRgejY0o9uxmD26n8tLjoMzk/VnKhB8BpaVc4WwfKaJ3RBkJesTYcwjKUS5sPAJyw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEEOP0C6ds6vuUtHrwQZjsNnPnz+QsJY/fIKkHU5KlquWV8IAJ9RWpqYrhf0MzxrcEA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEJhPgF+TObeNTJC8Ikb0fifsLzm4iPiGR11MV7wUcwzVnEd5olwvTSpJp15UIwZCwA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEPcGKr2pyMH7w45HDPHv7zyIq+KfBW5Y+NXA2AWY6yGTYBCn07WGkkhK7wJr5wDqaQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEGN/YF6zWYRu/Iq1WR85JXuk86sYw6WwjF1+NLGuV4ioW3uhtMq186foDASdmb0uIA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEN/EaHftFQzqraUiItb1kY7p8I+tvXGwZkCZLV+uzjNWBWLHPkbOoEo6usCvS6RFKQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEPEChHCilFfdDqJcbpVp3qcpW+kCESRYCrVJpuQlzyD0F+bIlhgUS1k2SUn5A1KMEA==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAELXaR1HGuqRlSt2jS93KQ0paIFvrN+dM8FHjKc2D9vUjyAPjxAhOiZPK3qWj+MWuzw==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEJ7NqlB0lhrY+Zr3Q4UixTlW3mgh8NFtGuYo088jhoOqzieaf4Vbtq9rc5PLfbURbg==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEDTHfRgKh2y8hLJwWHR5fp9O2kWZSimwqjUGFtQnQtSpEJgzC0bFAnsJ4kk3LS7wqw==");
+        }
+    }
+}
