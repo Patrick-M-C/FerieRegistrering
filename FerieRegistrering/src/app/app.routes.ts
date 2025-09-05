481795d69db294c8c776611176aa801013caf754
NiklasWeinreich <niklasweinreichhhansen@hotmail.com>
2025-09-05 14:35:09 +0200

    profl virker

--- FerieRegistrering/src/app/app.routes.ts
+++ FerieRegistrering/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './auth/login/login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { FerieComponent } from './ferie/ferie.component';
 import { HomeComponent } from './homepage/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'ferie', component: FerieComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
